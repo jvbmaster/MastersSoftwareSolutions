7c0b872942d1d3bdc26b8e78b2b43ada1a2e1e3b
Julio Vitor Barbieri <barbierijuliovitor@hotmail.com>
2018-12-11 13:02:05 -0200

    Inicio de Projeto Gerente de ativos para pacotes de Soluções "Masters Software Solutions"

--- AssetMananger/Form1.Designer.cs
+++ AssetMananger/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace AssetMananger
 {
-    partial class Form1
+    partial class FrmMain
     {
         /// <summary>
         /// Variável de designer necessária.
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "FrmMain";
+            this.Text = "Asset Manager";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
